ec950e1b4d571a599f18a100b02b7c785f24a526
ciggy <alexandermassietti@gmail.com>
2025-08-20 14:12:04 -0300

    entidades creadas: task, priority, status

--- Backend/microservice-task/src/app.module.ts
+++ Backend/microservice-task/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TaskModule } from './task/task.module';
+import { StatusModule } from './status/status.module';
+import { PriorityModule } from './priority/priority.module';
 
 @Module({
-  imports: [],
+  imports: [TaskModule, StatusModule, PriorityModule],
   controllers: [AppController],
   providers: [AppService],
 })
