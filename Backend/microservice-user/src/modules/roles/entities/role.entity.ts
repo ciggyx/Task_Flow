dc36134f0554cb58d522937411c7532de49b1da3
Julián <julianfermani45@gmail.com>
2025-08-22 10:13:43 -0300

    refactor: Rol + Conexión a DB

---
+++ Backend/microservice-user/src/modules/roles/entities/role.entity.ts
@@ -0,0 +1,34 @@
+// import { Permission } from 'src/permissions/entities/permission.entity';
+// import { UserEntity } from 'src/users/entities/user.entity';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('Rol')
+export class Role {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  code: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  // @ManyToMany(() => Permission, (permission) => permission.roles, {
+  //   eager: true,
+  // })
+  // @JoinTable()
+  // permissions: Permission[];
+  //
+  // @OneToMany(() => UserEntity, (user) => user.rol)
+  // users: UserEntity[];
+}
