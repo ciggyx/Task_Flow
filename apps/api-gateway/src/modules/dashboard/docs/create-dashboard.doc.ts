c0ecb5b9149ff47820459b4f51a2279875031001
Julian <julianfermani45@gmail.com>
2025-12-18 20:14:39 +0000

    chore(dashboard): añado documentación swagger

--- apps/api-gateway/src/modules/dashboard/docs/create-dashboard.doc.ts
+++ apps/api-gateway/src/modules/dashboard/docs/create-dashboard.doc.ts
@@ -8,11 +8,40 @@ export function CreateDashboardDoc() {
         ApiOperation({
             summary: 'Crear un nuevo dashboard',
         }),
+
         ApiBody({ type: CreateDashboardDto }),
+
         ApiResponse({
             status: 201,
             description: 'Dashboard correctamente creado',
             type: DashboardDto
         }),
+
+        ApiResponse({
+            status: 409,
+            description: 'El usuario ya tiene tiene un dashboard creado con ese nombre',
+            schema: {
+                example: {
+                    error: {
+                        statusCode: 409,
+                        message: "Repeated name on dashboard",
+                        details: null,
+                    },
+                },
+            },
+        }),
+
+        ApiResponse({
+            status: 401,
+            description: 'El usuario no está logueado',
+            schema: {
+                example: {
+                    statusCode: 401,
+                    error: "Unauthorized",
+                    message: "Missing Authorization header",
+                },
+            },
+        }),
+
     );
 }
